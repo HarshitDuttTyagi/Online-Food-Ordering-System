7cb3b9fc82d3e81771baf77ae4582f6feded6588
Harshit <harshitdutttyagi@gmail.com>
2024-05-20 13:54:18 +0530

    first commit

---
+++ admin/include/footer.php
@@ -0,0 +1,1 @@
+ <footer class="footer" style="text-align: center;"> Online Food Ordering System - <a href="https://github.com/HarshitDuttTyagi">Harshit Dutt Tyagi</a> </footer>
---
+++ admin/logout.php
@@ -0,0 +1,8 @@
+
+<?php
+session_start();
+session_destroy();
+$url = 'index.php';
+header('Location: ' . $url);
+
+?>
